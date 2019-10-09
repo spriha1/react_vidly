ce386d5f5abc25a05e5078bd3f4bf0c1304a5f88
Spriha <spriha@mindfiresolutions.com>
2019-10-09 18:17:48 +0530

    worked on routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import './App.css';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import Movies from './components/movies';
+import MovieForm from './components/movieForm';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
+import NavBar from './components/navBar';
+import './App.css';
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm}></Route>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+          {/* <Movies /> */}
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
