9657b70563d7fcab64d32d08007a179782dc7050
Spriha <spriha@mindfiresolutions.com>
2019-10-15 14:08:49 +0530

    added logout feature

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
-const NavBar = () => {
+const NavBar = ({ user }) => {
     return ( 
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <Link className="navbar-brand" to="/">Vidly</Link>
@@ -11,21 +11,19 @@ const NavBar = () => {
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/movies">Movies</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/customers">Customers</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/rentals">Rentals</NavLink>
-                    </li>
-                    <li className="nav-item">
+                    <NavLink className="nav-link" to="/movies">Movies</NavLink>
+                    <NavLink className="nav-link" to="/customers">Customers</NavLink>
+                    <NavLink className="nav-link" to="/rentals">Rentals</NavLink>
+                    {!user && 
+                    <React.Fragment>
                         <NavLink className="nav-link" to="/login">Login</NavLink>
-                    </li>
-                    <li className="nav-item">
                         <NavLink className="nav-link" to="/register">Register</NavLink>
-                    </li>
+                    </React.Fragment>}
+                    {user && 
+                    <React.Fragment>
+                        <NavLink className="nav-link" to="/profile">{user.name}</NavLink>
+                        <NavLink className="nav-link" to="/logout">Logout</NavLink>
+                    </React.Fragment>}
                 </ul>
             </div>
         </nav>
